cff416b8299cf3f4cd14d020e43361834fb61c9e
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-21 21:51:57 +0530

    Paragraph And Button Added To Test.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Mystic Math</h1>
+     <hr />
+
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolore optio id suscipit sapiente consequatur ab ea blanditiis maxime voluptatibus.</p>
+
+     <button>Click</button>
+    </>
+  )
+}
+
+export default App
