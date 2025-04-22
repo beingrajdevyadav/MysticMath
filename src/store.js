7f8854290df1a92986a883f27ecb0760b2678976
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-22 22:24:09 +0530

    Redux Store Configured.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import userReducer from "./redux/userSlice"
 
 export const store = configureStore({
     reducer: {
-        // Add your reducers here
+        user: userReducer,
     },
 })
