ba942846eae7d7b9320be42a6278fb2243d0a71b
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-22 22:03:48 +0530

    setUser() Added To Set And Save User Data.

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getFromLocalStorage } from "../utils/localStorage";
+import { getFromLocalStorage, saveToLocalStorage } from "../utils/localStorage";
 
 
 const localData = getFromLocalStorage("user");
@@ -17,3 +17,25 @@ const initialState =localData || {
   day: "",
 };
 
+const userSlice = createSlice({
+    name:"user",
+    initialState,
+
+    reducers:{
+        setUser : (state, action)=>{
+            // to set data in state
+            state.fullName = action.payload.fullName;
+            state.dateOfBirth = action.payload.dateOfBirth;
+            state.destinyNumber = action.payload.destinyNumber;
+            state.lifePathNumber = action.payload.lifePathNumber;
+            state.personalityNumber = action.payload.personalityNumber;
+            state.soulUrgeNumber = action.payload.soulUrgeNumber;
+            state.month = action.payload.month;
+            state.astroSign = action.payload.astroSign;
+            state.day = action.payload.day;
+
+            // to save state in local storage
+            saveToLocalStorage("user", state);
+        }
+    }
+})
