259a80e3cb141377c7782648de38d8ac854ab57e
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-23 19:58:46 +0530

    Hero Section Component Added.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className="hero-content">
+                <h1>Divine Digits</h1>
+                <hr />
+                <p>Let the ancient science of numerology decode your destiny. Start your journye with <b>MysticMath</b> today.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
