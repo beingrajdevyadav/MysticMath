8ec7ba9f3cdb294ea151ff3d79951134b9c13267
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-23 19:43:13 +0530

    Form Component Styled

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-
+import "../css/form.css"
 const Form = () => {
     const [fullName, setFullName] = useState("");
     const [dob, setDob] = useState("");
@@ -22,21 +22,25 @@ const Form = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h3>Dive In Mystic World  </h3>
-            <hr />
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <h3>Dive In Mystic World  </h3>
+                <hr />
 
-            <div className="input-container">
-                <input autoComplete='off' value={fullName} onChange={(e)=>setFullName(e.target.value)}  type="text" id='name' name='name' placeholder='Enter Full Name ' />
-            </div>
+                <div className="input-container">
+                    <input autoComplete='off' value={fullName} onChange={(e) => setFullName(e.target.value)} type="text" id='name' name='name' placeholder='Enter Full Name ' />
+                </div>
 
-            <div className="input-container">
-                <input value={dob} onChange={e=>setDob(e.target.value)} type="date" ref={dateInputRef} name="dob" id="dob" style={{display:"none"}} />
-                <button onClick={handleDatePicker} type='button'>Pick Date Of Birth</button>
-            </div>
+                <div className="input-container flex">
+                    <label htmlFor="">Pick Data Of Birth :</label>
+                    <input value={dob} onChange={e => setDob(e.target.value)} type="date" ref={dateInputRef} name="dob" id="dob" style={{ display: "none" }} />
+                    <button onClick={handleDatePicker} type='button'><i className="fa-solid fa-calendar-days"></i></button>
+                </div>
 
-            <button  type='submit'>Submit</button>
-        </form>
+                <button className='submit-btn' type='submit'>Submit</button>
+            </form>
+        </div>
+
     )
 }
 
