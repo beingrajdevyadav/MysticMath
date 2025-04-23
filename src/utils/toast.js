d547a7d072be9f7ac276831c01b748b9f5169135
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-23 21:54:47 +0530

    Info Toast Added.

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,18 +1,26 @@
 import { toast } from "react-toastify";
 
-export const showSuccessToast = (message)=>{
+export const showSuccessToast = (message) => {
     toast.success(message, {
-        position:"top-right",
-        autoClose:3000,
-        theme:"dark",
+        position: "top-right",
+        autoClose: 3000,
+        theme: "dark",
     });
 };
 
 
-export const showErrorToast = (message)=>{
+export const showErrorToast = (message) => {
     toast.error(message, {
-        position:"top-right",
-        autoClose:3000,
-        theme:"dark",
+        position: "top-right",
+        autoClose: 3000,
+        theme: "dark",
+    });
+};
+
+export const showInfoToast = (message) => {
+    toast.info(message, {
+        position: "top-right",
+        autoClose: 3000,
+        theme: "dark",
     });
 };
