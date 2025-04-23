decdbbdce8d0642c3522362ff043c2537c9f8fe0
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-23 10:41:27 +0530

    Circle 01 Styled And Animated.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,19 @@ function App() {
 
   return (
     <>
-     <header>
-      <h1>MysticMath</h1>
-     </header>
+      <header>
+        <div className='logo'>
+          <div className="icon">
+            <i class="fa-solid fa-wand-sparkles"></i>
+            <p> MysticMath</p>
+          </div>
+          
+
+          <div className="circle circle-1"></div>
+          <div className="circle circle-2"></div>
+          <div className="circle circle-3"></div>
+          </div>
+      </header>
     </>
   )
 }
