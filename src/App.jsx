fa5f9987fc1fe70b7d3ee7c37470fea1b601e6ec
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-23 12:41:12 +0530

    Preloader Component Added.

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,7 @@ function App() {
   return (
     <>
       <header>
-        <div className='logo'>
-          <div className="icon">
-            <i class="fa-solid fa-wand-sparkles"></i>
-            <p> MysticMath</p>
-          </div>
-          
-
-          <div className="circle circle-1"></div>
-          <div className="circle circle-2"></div>
-          <div className="circle circle-3"></div>
-          </div>
+       
       </header>
     </>
   )
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Preloader = () => {
+    return (
+        <>
+            <div className='logo'>
+                <div className="icon">
+                    <i class="fa-solid fa-wand-sparkles"></i>
+                    <p> MysticMath</p>
+                </div>
+
+                <div className="circle circle-1"></div>
+                <div className="circle circle-2"></div>
+                <div className="circle circle-3"></div>
+                <div className="circle circle-4"></div>
+            </div>
+        </>
+    )
+}
+
+export default Preloader
