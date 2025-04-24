6c51c535254930c715c6a791ccd1f2cf0bd1ba16
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-24 22:38:06 +0530

    getDestinyNumber() Added To Calculate Destiny Number.

--- src/utils/mystic.js
+++ src/utils/mystic.js
@@ -62,4 +62,21 @@ export const getLifePathNumber = (dd, mm, yyyy) => {
     const lifePathNumber = reduceToSingleDigit(day + month + year);
 
     return lifePathNumber;
-}
+};
+
+// Function to calculate destiny number
+export const getDestinyNumber = (fullName) => {
+    const alphabet = {
+        'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9,
+        'J': 1, 'K': 2, 'L': 3, 'M': 4, 'N': 5, 'O': 6, 'P': 7, 'Q': 8, 'R': 9,
+        'S': 1, 'T': 2, 'U': 3, 'V': 4, 'W': 5, 'X': 6, 'Y': 7, 'Z': 8
+    };
+
+    const nameValue = fullName.toUpperCase().split('').reduce((sum, char) => {
+        return sum + (alphabet[char] || 0);
+    }, 0);
+
+    return getLifePathNumber(nameValue);
+};
+
+
