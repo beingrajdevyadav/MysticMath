83231a6e7443a2b99a4c622bbd62524d0f063963
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-27 08:20:59 +0530

    home.css Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import "../css/home.css"
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className="home-banner">
+      <div className="home-banner-content">
+        <h1></h1>
+        <hr />
+        <p></p>
+      </div>
+    </div>
+      <h1>Welcome to Mystic Math</h1>
+
+    </>
   )
 }
 
