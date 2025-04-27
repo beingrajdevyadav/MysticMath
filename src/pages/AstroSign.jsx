76aea0d661c23817f43342159bef1ddc697c7f1a
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-27 16:47:16 +0530

    Luck Colors Section Added.

--- src/pages/AstroSign.jsx
+++ src/pages/AstroSign.jsx
@@ -4,21 +4,48 @@ import "../css/astrosign.css"
 import astro from '../assets/signs.json'
 
 const AstroSign = () => {
-  const report = useSelector((state)=>state.user);
+  const report = useSelector((state) => state.user);
   // console.log(astro)
 
-  const current = astro.filter((sign)=>sign.name == report.astroSign);
-  console.log(current);
+  const current = astro.filter((sign) => sign.name == report.astroSign)[0];
+  // console.log(current);
+
   return (
     <>
-    <div className="astro-banner">
-      <div className="astro-banner-content">
-        <h1>{report.astroSign}</h1>
-        <hr /> <br />
-        <p>Dear, {report.fullName}! </p>
-        <p>Here's your astro sign meaning.</p>
+      <div className="astro-banner">
+        <div className="astro-banner-content">
+          <h1>{report.astroSign}</h1>
+          <hr /> <br />
+          <p>Dear, {report.fullName}! </p>
+          <p>Here's your astro sign meaning.</p>
+        </div>
       </div>
-    </div>
+
+      <div className="astro-report">
+        <div className="astro-report-box">
+          <h2>{report.astroSign} Report</h2>
+          <hr />
+        </div>
+
+
+        <div className="astro-report-box">
+          <h3>Lucky Colours </h3>
+          <hr />
+
+          <ul>
+            {current.luckyColors.map(color => <li key={color}>{color}</li>)}
+          </ul>
+        </div>
+
+        
+        <div className="astro-report-box"></div>
+        <div className="astro-report-box"></div>
+        <div className="astro-report-box"></div>
+        <div className="astro-report-box"></div>
+        <div className="astro-report-box"></div>
+        <div className="astro-report-box"></div>
+
+      </div>
     </>
   )
 }
