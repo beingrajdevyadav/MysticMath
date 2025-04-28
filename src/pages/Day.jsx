85a80969b97974150fe282b5f0c80bbf556d5073
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-28 23:15:15 +0530

    Day Page Banner Completed.

--- src/pages/Day.jsx
+++ src/pages/Day.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Day = () => {
-  const report = useSelector((state)=>state.user);
+  const report = useSelector((state) => state.user);
+
   return (
     <>
-    <div className="day-banner">
+      <div className="day-banner">
+        <div className="day-banner-content">
+          <h1>{report.fullName}</h1>
+          <hr />
 
-    </div>
+          <p>Here's Divine Digits Predications Based On {report.day}</p>
+        </div>
+      </div>
     </>
   )
 }
