d3bded2891f925b712adff12e405146e94026bcb
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-28 23:33:30 +0530

    Day Page Predication Data Imported.

--- src/pages/Day.jsx
+++ src/pages/Day.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import data from "../assets/days.json";
 import "../css/day.css";
 
 
 const Day = () => {
   const report = useSelector((state) => state.user);
+const day = data.find((day) => day.name === report.day);
 
+console.log(day);
   return (
     <>
       <div className="day-banner">
