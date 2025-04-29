7b539ef80c3b9f1dc766160533847b43afd25047
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-29 19:35:28 +0530

    Month Report Page Data Filtered .

--- src/pages/Month.jsx
+++ src/pages/Month.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import data from '../assets/months.json'
+import { useSelector } from 'react-redux'
 const Month = () => {
+  const monthName = useSelector((state)=>state.user.month);
+
+const report = data.find(r=>r.name === monthName);
+console.log(report)
   return (
-    <div>Month</div>
+    <>
+<div className="month-banner">
+  <div className="month-banner-content">
+    <h1>{}</h1>
+  </div>
+</div>
+
+    </>
   )
 }
 
