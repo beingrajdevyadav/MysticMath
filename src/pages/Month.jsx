7e8bffa4c8016162109bd262c59402c141801487
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-29 19:51:53 +0530

    Month Report Page Banner Styled.

--- src/pages/Month.jsx
+++ src/pages/Month.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import data from '../assets/months.json'
 import { useSelector } from 'react-redux'
-const Month = () => {
-  const user = useSelector((state)=>state.user);
+import '../css/month.css'
 
-const report = data.find(r=>r.name === user.month);
-console.log(report)
+
+
+const Month = () => {
+  const user = useSelector((state) => state.user);
+
+  const report = data.find(r => r.name === user.month);
+  console.log(report)
   return (
     <>
-<div className="month-banner">
-  <div className="month-banner-content">
-    <h1>{report.name}</h1>
-    <hr />
+      <div className="month-banner">
+        <div className="month-banner-content">
+          <h1>{report.name}</h1>
+          <hr />
+          <br />
 
-    <br />
-    <p>Dear, {user.fullName}</p>
-    <p>DivineDigits Month Report For You</p>
-  </div>
-</div>
+          <p>Dear, {user.fullName}</p>
+          <p>DivineDigits Month Report For You</p>
+        </div>
+      </div>
 
     </>
   )
