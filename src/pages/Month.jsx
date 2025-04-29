5a590dded5d05ac1923fc322cc6dcdbe76b71b50
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-04-29 19:40:31 +0530

    Month Report Page Banner Content Added.

--- src/pages/Month.jsx
+++ src/pages/Month.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import data from '../assets/months.json'
 import { useSelector } from 'react-redux'
 const Month = () => {
-  const monthName = useSelector((state)=>state.user.month);
+  const user = useSelector((state)=>state.user);
 
-const report = data.find(r=>r.name === monthName);
+const report = data.find(r=>r.name === user.month);
 console.log(report)
   return (
     <>
 <div className="month-banner">
   <div className="month-banner-content">
-    <h1>{}</h1>
+    <h1>{report.name}</h1>
+    <hr />
+
+    <br />
+    <p>Dear, {user.fullName}</p>
+    <p>DivineDigits Month Report For You</p>
   </div>
 </div>
 
