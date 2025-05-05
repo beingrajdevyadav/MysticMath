f9aa08422bae1255621e508fe7bf98a4877ed4e9
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-05-05 23:14:37 +0530

    Report Data Got.

--- src/pages/LifePathNumber.jsx
+++ src/pages/LifePathNumber.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import data from '../assets/lifepaths.json'
+import '../css/life.css'
 
 const LifePathNumber = () => {
+  console.log(data);
   return (
     <>
-<div className="life-banner">
-  <div className="life-banner-content">
-    <h1>Life Path Number</h1>
-    <hr />
-    <p>Discover your life path number and its significance.</p>
-  </div>
-</div>
+      <div className="life-banner">
+        <div className="life-banner-content">
+          <h1>Life Path Number</h1>
+          <hr />
+          <p>Discover your life path number and its significance.</p>
+        </div>
+      </div>
+
+      <div className="life-report">
+        <div className="life-report-box">
+          <h3>Life Path Report</h3>
+          <hr />
+        </div>
+
+        
+
+      </div>
     </>
   )
 }
